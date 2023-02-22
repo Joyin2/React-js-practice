c0cf3a31214c3bcb889759a8c26e595ead05c17d
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-22 20:10:07 +0530

    use effect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 // import UseStateToggle from './toggle/UseStateToggle'
 // import UseStateArray from "./hooks/UseStateArray"
 import Covid from './components/Covid'
-import UseEffect from './hooks/UseEffect'
+import UseEffect from './hooks/UseEffect/UseEffect'
 // import "./App.css"
 
 
