a1f8a2e2befb8fbaadf0ce5c6cf0b95325ce8b6e
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-22 10:24:03 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react'
 import UseState from './hooks/UseState'
 import UseStateToggle from './toggle/UseStateToggle'
 import UseStateArray from "./hooks/UseStateArray"
-import "./App.css"
+// import "./App.css"
 
 
 const App = () => {
 
   return (
     <div className='App'>
+      hello
       {/* <UseState/> */}
       {/* <UseStateToggle/> */}
-      <UseStateArray/>
+      {/* <UseStateArray/> */}
     </div>
   )
 }
--- src/hooks/UseStateArray.js
+++ src/hooks/UseStateArray.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const UseStateArray = () => {
+    const bioData = [
+      {
+        id:0, myName:"Joyin", age: 22
+      },
+      {
+        id:1, myName:"Mahmmad", age: 23
+      },
+      {
+        id:2, myName:"Aslam", age: 24
+      }
+    ]
+
   return (
     <div>
-      UseStateArray
+      {}
     </div>
   )
 }
