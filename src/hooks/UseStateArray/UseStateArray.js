6b80d9f8b88f0a51ba5eeb297ac62e2b061831fe
Joyin2 <joyinlaskar2000@gmail.com>
2023-03-12 19:00:12 +0530

    use state array

--- src/hooks/UseStateArray/UseStateArray.js
+++ src/hooks/UseStateArray/UseStateArray.js
@@ -1,24 +1,41 @@
-import React from 'react'
-import "./UseStateArray.css"
+import React, { useState } from "react";
+import "./UseStateArray.css";
 
 const UseStateArray = () => {
-    const bioData = [
-      {
-        id:0, myName:"Joyin", age: 22
-      },
-      {
-        id:1, myName:"Mahmmad", age: 23
-      },
-      {
-        id:2, myName:"Aslam", age: 24
-      }
-    ]
+  const bioData = [
+    {
+      id: 0,
+      myName: "Joyin",
+      age: 22,
+    },
+    {
+      id: 1,
+      myName: "Mahmmad",
+      age: 22,
+    },
+    {
+      id: 2,
+      myName: "Aslam",
+      age: 22,
+    },
+  ];
+  const [Array, setArray] = useState(bioData);
+  const clearArray = () => {
+    setArray([]);
+  };
 
   return (
-    <div>
-      hello
-    </div>
-  )
-}
+    <>
+      {
+        Array.map((curr) => {
+          return (
+            <h1 key={curr.id}>Name: {curr.myName}, Age: {curr.age}</h1>
+          )
+        })
+      }
+      <button onClick={clearArray}>Clear</button>
+    </>
+  );
+};
 
-export default UseStateArray
+export default UseStateArray;
