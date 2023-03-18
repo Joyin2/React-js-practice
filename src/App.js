9b6b77f1adaeb19788f7effd8771d8b0b3e3cb56
Joyin2 <joyinlaskar2000@gmail.com>
2023-03-18 21:12:59 +0530

    form validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React,{createContext} from "react";
+import React, { createContext } from "react";
 // import SpreadOperator from "./hooks/SpreadOperatorInUseState/SpreadOperator";
 // import UseState from './hooks/UseState'
 // import UseStateToggle from './toggle/UseStateToggle'
 // import UseStateArray from "./hooks/UseStateArray/UseStateArray";
 // import ShortCirtcuitEvaluation from "./components/ShortCircuitEval/ShortCirtcuitEvaluation";
-// import Login from "./components/LoginForm/Login";
+import Login from "./components/LoginForm/Form";
 // import Covid from "./components/Covid";
 // import UseEffect from "./hooks/UseEffect/UseEffect";
 // import CleanUp from "./hooks/UseEffect/CleanUp";
@@ -14,17 +14,15 @@ import React,{createContext} from "react";
 // import NetflixApp from "./components/NetflixApp";
 // import SlotMachine from "./components/SlotMachineGame"
 // import IncreDecre from "./hooks/IncreDecre/index"
-import ComA from "./hooks/ContextApi/ComA";
-import ComB from "./hooks/ContextApi/ComB";
+// import ComA from "./hooks/ContextApi/ComA";
+// import ComB from "./hooks/ContextApi/ComB";
 
 const App = () => {
   return (
     <>
-        <ComB />
+      <Login />
     </>
   );
 };
 
 export default App;
-
-
