036520accb8ed4d297ab12bf486d473a53ea0f2a
Joyin2 <joyinlaskar2000@gmail.com>
2023-04-01 22:53:21 +0530

    simple forms

--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -1,23 +1,30 @@
-import React, { useState } from 'react'
-import "./Forms.scss"
+import React, { useState } from "react";
+import "./Forms.scss";
 
 const Forms = () => {
-  const [name, setName] = useState("")
-  const [fullName, setFullName] = useState("")
-  const inputEvent = (event)=>{
+  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState("");
+  const inputEvent = (event) => {
     // console.log(event.target.value)
     setName(event.target.value);
-  }
-  const onSubmit =()=>{
-    setFullName(name)
-  }
+  };
+  const onSubmit = () => {
+    setFullName(name);
+  };
   return (
-    <div className='Forms'>
+    <div className="Forms">
       <h1>Hello {fullName}</h1>
-      <input type="text" name="" id="" placeholder='enter your name' onChange={inputEvent} value={name}/>
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="enter your name"
+        onChange={inputEvent}
+        value={name}
+      />
       <button onClick={onSubmit}>Click</button>
     </div>
-  )
-}
+  );
+};
 
-export default Forms
+export default Forms;
