c4dcbdaa4fe42158c6900968ea768bc2768618c4
Joyin2 <joyinlaskar2000@gmail.com>
2023-04-07 22:48:06 +0530

    form

--- src/components/Forms/Forms3.0/Form.jsx
+++ src/components/Forms/Forms3.0/Form.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Form.scss";
 
 const Form = () => {
+  const [userRegistration, setUserRegistration] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+  const handleInput = () => {};
   return (
-    <div>
-      forms coming soon ...
-    </div>
-  )
-}
+    <>
+      <form action="">
+        <div>
+          <label htmlFor="username">Fullname</label>
+          <input
+            type="text"
+            autoComplete="off"
+            value=""
+            onChange={handleInput}
+            name="username"
+            id="username"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            autoComplete="off"
+            value=""
+            onChange={handleInput}
+            name="email"
+            id="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone</label>
+          <input
+            type="text"
+            autoComplete="off"
+            value=""
+            onChange={handleInput}
+            name="phone"
+            id="phone"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            autoComplete="off"
+            value=""
+            onChange={handleInput}
+            name="password"
+            id="password"
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
 
-export default Form
+export default Form;
