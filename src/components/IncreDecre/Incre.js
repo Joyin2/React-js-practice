2f794cac3498783463d5ed30219f02259ba91fb1
Joyin2 <joyinlaskar2000@gmail.com>
2023-04-09 14:19:31 +0530

    used material ui

--- src/components/IncreDecre/Incre.js
+++ src/components/IncreDecre/Incre.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import Button from "@mui/material/Button";
 
 const Incre = () => {
-  const [number, setNumber] = useState(0)
-  const IncNum = () =>{
-    setNumber(number+1)
-    
-  }
-  const DecNum = ()=>{
-    
-    if (number<=0) {
-      return alert("zero limit reached")
-    }else{
+  const [number, setNumber] = useState(0);
+  const IncNum = () => {
+    setNumber(number + 1);
+  };
+  const DecNum = () => {
+    if (number <= 0) {
+      return alert("zero limit reached");
+    } else {
       setNumber(number - 1);
     }
-  }
+  };
   return (
     <>
       <h1>{number}</h1>
-      <button onClick={IncNum} >Click to increase</button>
-      <button onClick={DecNum} >Click to decrease</button>
+      <Button variant="outlined" onClick={IncNum}>
+        <AddCircleIcon sx={{ fontSize: 40 }} />
+      </Button>
+      <span onClick={DecNum}>
+        <RemoveCircleIcon style={{ color: "red" }} sx={{ fontSize: 40 }} />
+      </span>
     </>
-  )
-}
+  );
+};
 
-export default Incre
+export default Incre;
